6a1de5fe6729e1d4a0719f7547b564a97c2d1f68
Gordon Yu <gordon@gordonyu.com>
2018-01-17 15:16:41 -0800

    ispalindrome done

--- ispalindrome.js
+++ ispalindrome.js
@@ -1,15 +1,33 @@
 isPalindrome = (string) => {
-    alpha = 'abcdefghijklmnopqrstuvwxyz'
-    char = string.split(' ').join('').toLowerCase()
+  alpha = 'abcdefghijklmnopqrstuvwxyz'
+  char = string.split(' ').join('').toLowerCase()
 
-    onlyLetters = char.split('').filter(letter => {
-        return alpha.split('').indexOf(letter) > 0
-    })
+  onlyLetters = char.split('').filter(letter => {
+    return alpha.split('').indexOf(letter) > 0
+  })
 
-    console.log(onlyLetters === onlyLetters.reverse())
+  console.log(onlyLetters === onlyLetters.reverse())
 }
 
 
 
 // isPalindrome('race car')
-isPalindrome('Madam, Im Adam')
+// isPalindrome('Madam, Im Adam')
+
+
+function isPalindrome2(string) {
+  string = string.toLowerCase();
+  var charactersArr = string.split('');
+  var validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  var lettersArr = [];
+  charactersArr.forEach(char => {
+    if (validCharacters.indexOf(char) > -1) lettersArr.push(char);
+  })
+
+  if (lettersArr.join('') === lettersArr.reverse().join('')) return true;
+  else return false;
+
+}
+
+isPalindrome2('Madam, Im Adam')
